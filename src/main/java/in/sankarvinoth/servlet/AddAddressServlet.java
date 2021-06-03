fe5f6dda66683d170bfaa66b7532ffca3fca51b5
SankarVinoth <vinothsankar885@gmail.com>
2021-06-03 18:09:01 +0530

    Add address Feature

---
+++ src/main/java/in/sankarvinoth/servlet/AddAddressServlet.java
@@ -0,0 +1,26 @@
+package in.sankarvinoth.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import in.sankarvinoth.dao.UserDao;
+
+/**
+ * Servlet implementation class AddAddressServlet
+ */
+@WebServlet("/AddAddressServlet")
+public class AddAddressServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+  
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String Username=request.getParameter("username");
+		String address=request.getParameter("address");
+		UserDao.updateUserAddress(address,Username);
+		
+	}
+
+}
